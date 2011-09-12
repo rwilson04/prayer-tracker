dd2e7b7bdf4e3eae55fa409b385d454e03a3688d
Reese Wilson <rwilson04+github@gmail.com>
2011-09-12 22:26:07 +0000

    added layouts, starting authentication
    authentication in bootstrap needs an acl

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -16,5 +16,38 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 		return $autoloader;
 	}
 
+	//protected function _initPlugin()
+	//{
+		//$fc = Zend_Controller_Front::getInstance();
+		//$fc->registerPlugin(new Shinymayhem_Plugin_PluginName());
+	//}
+	
+	protected function _initAuthentication()
+	{
+	  $fc = Zend_Controller_Front::getInstance();
+	  $plugin = new Shinymayhem_Plugin_Authentication();
+	  $plugin->loginModule = "default";
+	  $plugin->loginController = "users";
+	  $plugin->loginAction = "login";
+	  $plugin->modular = true;
+	  //$plugin->acl = $acl;
+	  $fc->registerPlugin($plugin);
+	}
+
+	protected function _initDebugPlaceholder()
+	{
+		$this->bootstrap('view');
+		$view = $this->getResource('View');
+		$view->placeholder('debug')
+			->setPrefix("<pre>")
+			->setSeparator("<br />")
+			->setPostfix("</pre>");
+	}
+
+	protected function _initDebugRegistry()
+	{
+		Zend_Registry::set('debug', null);
+	}
+									
 }
 
