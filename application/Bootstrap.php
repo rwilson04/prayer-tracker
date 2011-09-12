d5ff26f2fe0f9b7af0fc98b382e9dbb2057e1e5a
Reese Wilson <rwilson04+github@gmail.com>
2011-09-12 17:15:53 +0000

    starting to set up environment
    initialized views resource
    initialized autoloader

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -2,7 +2,19 @@
 
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
+	protected function _initDoctype()
+	{
+		$this->bootstrap('view');
+		$view = $this->getResource('view');
+		$view->doctype('HTML5');
+	}
 
+	protected function _initAutoload()
+	{
+		$autoloader = Zend_Loader_Autoloader::getInstance();
+		$autoloader->registerNamespace(array('Shinymayhem_'));
+		return $autoloader;
+	}
 
 }
 
